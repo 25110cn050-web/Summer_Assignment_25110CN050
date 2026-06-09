b2aec4476383a82adf559d2fad3647e298fbff08
25110cn050-web <25110cn050@glbitm.ac.in>
2026-06-09 12:55:58 +0530

    Create Que_1.java

---
+++ Day-1/Que_1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class pattern_7 {
+    public static void main(String args[]){
+        System.out.println("Enter the value of n");
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int sum = n*(n+1)/2;
+        System.out.println(sum);
+    }}
+    
