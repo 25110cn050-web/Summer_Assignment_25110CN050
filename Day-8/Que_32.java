2946db13333a667b46d56420bfa95a4ca52262a4
25110cn050-web <25110cn050@glbitm.ac.in>
2026-06-24 21:05:00 +0530

    Create Que_32.java

---
+++ Day-8/Que_32.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class assign_32 {
+    //Write a program to Print repeated-number pattern. 
+    public static void main(String args[]){
+        System.out.println("Enter the number of row");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=i ; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+    }
+}
+
