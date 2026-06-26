7836c1d2dd28195232c2e4b811642a79474e3e30
25110cn050-web <25110cn050@glbitm.ac.in>
2026-06-26 10:10:50 +0530

    Create Que_33.java

---
+++ Day-9/Que_33.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class assign_33 {
+    //Write a program to Print reverse star pattern.
+    public static void main(String args[]){
+        System.out.println("Enter the number of row");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=n ; i>=1; i--){
+            for(int j=1 ; j<=i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
