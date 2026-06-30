7b4172b669b9eeb8fc3e43a04a2a117d5f8e01fc
25110cn050-web <25110cn050@glbitm.ac.in>
2026-07-01 01:37:43 +0530

    Create Que_55.java

---
+++ Day-14/Que_55.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class assign_55 {
+    //Write a program to find Second largest element. 
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number of element");
+    int n = sc.nextInt();
+    int arr[] = new int[n];
+    System.out.println("Enter the number of element");
+    for(int i=0 ; i<n ; i++){
+        arr[i] = sc.nextInt();
+    }
+    int largest = arr[0];
+    int second_largest = arr[0];
+    for(int i=0 ; i<n ; i++){
+        if(largest<arr[i]){
+        second_largest = largest;
+        largest = arr[i];
+        }
+        else if(arr[i]>second_largest || arr[i]!=largest){
+            second_largest=arr[i];
+        }
+    }
+    System.out.println("The second largest element is "+second_largest);
+}
+    
+    
+}
