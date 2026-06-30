2b17a0f035d122c0e1200ff724f1c9cefb24d14c
25110cn050-web <25110cn050@glbitm.ac.in>
2026-07-01 01:34:45 +0530

    Create Que_52.java

---
+++ Day-13/Que_52.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class assign_52 {
+//Write a program to Count even and odd elements.  
+public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number of element");
+    int n = sc.nextInt();
+    int arr[] = new int[n];
+    int even =0 , odd = 0;
+    System.out.println("Enter the number of element");
+    for(int i=0 ; i<n ; i++){
+        arr[i] = sc.nextInt();
+    }
+    for(int i=0 ; i<n ; i++){
+    if(arr[i]%2==0){
+        even++;
+    }else{
+        odd++;
+    }
+    }
+    System.out.println("The number of even element is "+even);
+    System.out.println("The number of odd element is "+odd);
+}
+}
