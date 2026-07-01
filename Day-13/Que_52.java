e7445f5db4da5980f4e4ce6c1a952b2f76e7343c
25110cn050-web <25110cn050@glbitm.ac.in>
2026-07-01 11:38:47 +0530

    Update Que_52.java

--- Day-13/Que_52.java
+++ Day-13/Que_52.java
@@ -7,7 +7,7 @@ public static void main(String args[]){
     int n = sc.nextInt();
     int arr[] = new int[n];
     int even =0 , odd = 0;
-    System.out.println("Enter the number of element");
+    System.out.println("Enter the element of array");
     for(int i=0 ; i<n ; i++){
         arr[i] = sc.nextInt();
     }
