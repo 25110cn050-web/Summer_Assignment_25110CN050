085db692b7668aeb2830bac6a328c173a90e1a38
25110cn050-web <25110cn050@glbitm.ac.in>
2026-07-01 11:40:38 +0530

    Update Que_55.java

--- Day-14/Que_55.java
+++ Day-14/Que_55.java
@@ -6,7 +6,7 @@ public class assign_55 {
     System.out.println("Enter the number of element");
     int n = sc.nextInt();
     int arr[] = new int[n];
-    System.out.println("Enter the number of element");
+    System.out.println("Enter the element of array");
     for(int i=0 ; i<n ; i++){
         arr[i] = sc.nextInt();
     }
